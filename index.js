6dab662553f003940349aa3af032aea4e8ff772a
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-28 21:48:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * expand-target <https://github.com/jonschlinkert/expand-target>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
