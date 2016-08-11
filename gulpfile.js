0c3c38c8699523477ae45e009708b434b91d2690
doowb <brian.woodward@gmail.com>
2016-08-11 19:41:04 -0400

    remove test files from code coverage include utils.js in code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
 
-var lint = ['index.js', 'test/*.js'];
+var lint = ['index.js', 'utils.js'];
 
 gulp.task('coverage', function() {
   return gulp.src(lint)
